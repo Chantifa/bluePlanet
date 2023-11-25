ad03a47de0565f1b6dee711f9672daf66ba5fba6
dobbyderelf <151916594+dobbyderelf@users.noreply.github.com>
2023-11-25 11:51:16 +0100

    Update DDL.sql
    add data

--- backend/database/DDL.sql
+++ backend/database/DDL.sql
@@ -28,3 +28,27 @@ CREATE TABLE Schule (
                         Weiteres VARCHAR (500)
 
 );
+
+
+INSERT INTO Schule (
+    Name,
+    Hauptenergietraeger,
+    Hauptenergietraeger_Verbrauch,
+    Stromverbrauch,
+    Stromprodukt,
+    Stromanbieter,
+    CO2_Emission,
+    Photovolaikanlage,
+    Photovoltaikanlage_Flaeche,
+    Photovolaikanlage_Leistung,
+    Photovolaikanlage_Produktion,
+    Miergie,
+    Anzahl_Schueler,
+    Schulhaus_Flaeche
+)
+VALUES
+    ('Schule Gockhausen ZH', 'Heizöl', 20000, 50000, 'Naturstrom', 'ewz Elektrizitätswerk der Stadt Zürich', 0.01, '1', 60, 140, 1260, 0, 80, 170),
+    ('Freie Schule Winterthur ZH', 'Wärmepumpe', 45000, 80000, 'Standardstrom', 'Elektrizitätswerke des Kantons Zürich - EKZ', 0.8, 0, NULL, NULL, NULL, '1', 120, 200),
+    ('Sekundarschule Gelterkinden BL', 'Biogas', 50000, 100000, 'Ökostrom', 'Alpiq AG', 0.45, 'Ja', 100, 153, 2300, '1', 200, 250),
+    ('Sekundarschule Hüenerweid Dietlikon ZH', 'Heizöl', 35000, 93000, 'Naturstrom', 'Werke am Zürichsee AG', 0.02, 0, NULL, NULL, NULL, 0, 180, 240),
+    ('Sekundarschule Seuzach ZH', 'Holzpellets/Schnitzel', 32000, 120000, 'Standardstrom', 'Elektrizitätswerke des Kantons Zürich - EKZ', 0.7, 0, NULL, NULL, NULL, '1', 200, 300);
